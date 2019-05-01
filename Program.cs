916ec4427cd4188c75aa7caac4e82c2105c9503f
Arash Outadi <arash.out@gmail.com>
2019-04-30 19:41:54 -0700

    small formatting

--- Program.cs
+++ Program.cs
@@ -26,20 +26,22 @@ namespace SudokuSolve {
                 System.Console.WriteLine ("{0} in {1}", gridStrings.Count, fileName);
 
                 // TODO: Do initial solve to reduce jitter
-
                 Console.WriteLine ("Starting benchmarking");
                 var solveTimes = new List<double> (new double[gridStrings.Count]);
+                
                 Stopwatch sw = new Stopwatch ();
                 int numIterations = 10;
+                
                 for (int i = 0; i < numIterations; i++) {
                     for (int j = 0; j < gridStrings.Count; j++) {
                         var cpb = new CPBoard (gridStrings[j]);
                         cpb.Print ();
-
                         Console.WriteLine (new String ('-', 20));
+                        
                         sw.Restart ();
                         var sr = CPSolver.Solve (cpb);
                         sw.Stop ();
+                        
                         double ticks = sw.ElapsedTicks;
                         double seconds = ticks / Stopwatch.Frequency;
                         double milliseconds = (ticks / Stopwatch.Frequency) * 1000;
@@ -48,9 +50,11 @@ namespace SudokuSolve {
                         if (!sr.DidSolve) {
                             throw new Exception ($"Could not solve:\n {cpb.ToString()}");
                         }
+                        
                         sr.CPB.Print ();
                     }
                 }
+                
                 var averageTimes = solveTimes.Select (st => st / numIterations).ToList ();
                 var averageTime = averageTimes.Average ();
                 Console.WriteLine ($"Average solve time was: {averageTime}");
@@ -71,6 +75,7 @@ namespace SudokuSolve {
                         var cpb = new CPBoard (line);
                         cpb.Print ();
                         Console.WriteLine (new String ('-', 20));
+                        
                         var sr = CPSolver.Solve (cpb);
                         if (!sr.DidSolve) {
                             throw new Exception ($"Could not solve:\n {cpb.ToString()}");
